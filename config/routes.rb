0b4de507867ebd4fe2b03b5bc9b892425cbb66e6
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-06-26 23:56:15 -0400

    Added Basic Routes & Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # Generic syntax:
+  # verb 'path', to: 'controller#action', as: :route_name
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
