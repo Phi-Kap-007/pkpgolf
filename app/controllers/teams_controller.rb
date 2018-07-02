9425381b12dffc94db88effaf6dba189055a40c6
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-02 00:32:25 -0400

    Confusion with Params & Index in Controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,15 +1,20 @@
 class TeamsController < ApplicationController
 
+# Put teams in Database when the time is right
+
   TEAMS = {
-    Team_1 => { name: "Golden Oldies", members: ["Chris Mapp", "Mark H.", "Jim Ross", "Bill Gregory"]},
-    Team_2 => { name: "SP Caravan", members: ["Christian Chan", "Ardavan Hosseini", "Gianni Macri", "John-Michael Aita"]},
-    Team_3 => { name: "Shore Whores", members: ["Troy Huzulak", "Derek Ruediger", "Jared Wielkanoc", "Pat Therrien"]},
-    Team_4 => { name: "House Crew", members: ["Bacon Forte", "Grayson Castell", "Jack Tisdell", "Wiliiam Ducasse"]},
-    Team_5 => { name: "Middle-Aged", members: ["Carlos Da Costa Frias", "Thomas Dayon", "Trevor Lee", "Fred St-Amour"]},
-    Team_6 => { name: "Peter Corrado's Party Foursome", members: ["Alex Corrado", "David Gao", "Michel Toutoungy", "Ryan Kucera"]}
+    1 => { name: "Golden Oldies", members: ["Chris Mapp", "Mark H.", "Jim Ross", "Bill Gregory"]},
+    2 => { name: "SP Caravan", members: ["Christian Chan", "Ardavan Hosseini", "Gianni Macri", "John-Michael Aita"]},
+    3 => { name: "Shore Whores", members: ["Troy Huzulak", "Derek Ruediger", "Jared Wielkanoc", "Pat Therrien"]},
+    4 => { name: "House Crew", members: ["Bacon Forte", "Grayson Castell", "Jack Tisdell", "Wiliiam Ducasse"]},
+    5 => { name: "Middle-Aged", members: ["Carlos Da Costa Frias", "Thomas Dayon", "Trevor Lee", "Fred St-Amour"]},
+    6 => { name: "Peter Corrado's Party Foursome", members: ["Alex Corrado", "David Gao", "Michel Toutoungy", "Ryan Kucera"]}
   }
 
   def index
     @teams = TEAMS
+    # .select do |id, team|
+    #   team[:members] == params [:member_name]
+    # end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   # Generic syntax:
   # verb 'path', to: 'controller#action', as: :route_name
+
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'contact', to: 'pages#contact', as: :contact
