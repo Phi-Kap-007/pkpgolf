f4989454f6035bdae67cc3b71389531af652d64c
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-04 11:02:23 -0400

    More Database Action

--- db/migrate/20180704144913_create_teams.rb
+++ db/migrate/20180704144913_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.2]
     create_table :teams do |t|
       t.string :name
       t.integer :tee_off_time
+      t.integer :score
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_130859) do
+ActiveRecord::Schema.define(version: 2018_07_04_144913) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
-    t.time "tee_off_time"
+    t.integer "tee_off_time"
+    t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
