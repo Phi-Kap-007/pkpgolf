b4bc8d750c6735686c4abbde9baf0f45321de2aa
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-04 00:19:17 -0400

    Pre Potential Screwup with PostgresQL

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,12 +6,13 @@ class TeamsController < ApplicationController
     1 => { name: "Golden Oldies", members: ["Chris Mapp", "Mark H.", "Jim Ross", "Bill Gregory"]},
     2 => { name: "SP Caravan", members: ["Christian Chan", "Ardavan Hosseini", "Gianni Macri", "John-Michael Aita"]},
     3 => { name: "Shore Whores", members: ["Troy Huzulak", "Derek Ruediger", "Jared Wielkanoc", "Pat Therrien"]},
-    4 => { name: "House Crew", members: ["Bacon Forte", "Grayson Castell", "Jack Tisdell", "Wiliiam Ducasse"]},
+    4 => { name: "House Crew", members: ["Bacon Forte", "Grayson Castell", "Jack Tisdell", "William Ducasse"]},
     5 => { name: "Middle-Aged", members: ["Carlos Da Costa Frias", "Thomas Dayon", "Trevor Lee", "Fred St-Amour"]},
     6 => { name: "Peter Corrado's Party Foursome", members: ["Alex Corrado", "David Gao", "Michel Toutoungy", "Ryan Kucera"]}
   }
 
   def index
+    # Go to video around 1:09.00 mark, having issues getting the form to work properly
     @teams = TEAMS
     # .select do |id, team|
     #   team[:members] == params [:member_name]
