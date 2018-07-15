861e2596c9359caab432cdab23f7b8a5290511d3
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-15 16:45:25 -0400

    Added Comment to User.rb File

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # If you want to do private beta (where users cannot sign up), remove the 'registerable' symbol
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
