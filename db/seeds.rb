acccb2759d176ede176224621b23e5cd4934b8bf
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-21 22:50:24 -0400

    Need Help With the New Seed, After All Recent Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,16 @@ Team.destroy_all
 
 puts 'Creating teams...'
 
-Team.create(name: "Golden Oldies", members: "Chris Mapp" "Mark Uchwat" "Jim Ross" "Bill Gregory", tee_off_time: 930, score: 0)
-Team.create(name: "Uchwat Family", members: "Unknown 1" "Unknown 2" "Unknown 3" "Unknown 4", tee_off_time: 938, score: 0)
-Team.create(name: "The Organizers", members: "Arthur Burczak" "Sebastien Jacques" "Derek Pickford" "LJ Aguinaga", tee_off_time: 946, score: 0)
-Team.create(name: "Mish-Mash", members: "Alex Corrado" "Carlos Da Costa Frias" "Cody Lieberman" "Colin Mackenzie", tee_off_time: 954, score: 0)
-Team.create(name: "Order of Former CPA's of Quebec", members: "Artour Goldenberg" "Pascal Karel Matte" "Cody Lieberman" "Colin Mackenzie", tee_off_time: 1002, score: 0)
-Team.create(name: "SP Caravan", members: "Benjamin Mason" "Ardavan Hosseini" "Adrian Payne" "Sajith Rupasingha", tee_off_time: 1010, score: 0)
-Team.create(name: "The Swordsmiths", members: "Yasin Ismail" "Alex Le" "Nicholas Jasinski" "Alexandre Duchesne", tee_off_time: 1018, score: 0)
-Team.create(name: "SP Caravan, The Sequel", members: "Andrew Brand" "Arun Aggarwal-Schifellite" "Christian Chan" "Scott McGee", tee_off_time: 1026, score: 0)
-Team.create(name: "House Crew", members: "James Fraser" "Ryan Kucera" "Jack Tisdell" "William Ducasse", tee_off_time: 1034, score: 0)
-Team.create(name: "Alcoholics Anonymous", members: "Dario Baccalaro" "Shane Chappus" "Noah Gareau" "N/A", tee_off_time: 1042, score: 0)
+Team.create(name: "Golden Oldies", members: [[first_name: "Chris" last_name: "Mapp"], [first_name: "Mark" last_name: "Uchwat"], [first_name: "Jim" last_name: "Ross"], [first_name: "Bill" last_name: "Gregory"]], tee_off_time: 930, score: 0)
+# Team.create(name: "Uchwat Family", members: ["Unknown 1" "Unknown 2" "Unknown 3" "Unknown 4"], tee_off_time: 938, score: 0)
+# Team.create(name: "The Organizers", members: ["Arthur Burczak" "Sebastien Jacques" "Derek Pickford" "LJ Aguinaga"], tee_off_time: 946, score: 0)
+# Team.create(name: "Mish-Mash", members: ["Alex Corrado" "Carlos Da Costa Frias" "Cody Lieberman" "Colin Mackenzie"], tee_off_time: 954, score: 0)
+# Team.create(name: "Order of Former CPA's of Quebec", members: ["Artour Goldenberg" "Pascal Karel Matte" "Kirby Coussa" "David J. Stein"], tee_off_time: 1002, score: 0)
+# Team.create(name: "SP Caravan", members: ["Benjamin Mason" "Ardavan Hosseini" "Adrian Payne" "Sajith Rupasingha"], tee_off_time: 1010, score: 0)
+# Team.create(name: "The Swordsmiths", members: ["Yasin Ismail" "Alex Le" "Nicholas Jasinski" "Alexandre Duchesne"], tee_off_time: 1018, score: 0)
+# Team.create(name: "SP Caravan, The Sequel", members: ["Andrew Brand" "Arun Aggarwal-Schifellite" "Christian Chan" "Scott McGee"], tee_off_time: 1026, score: 0)
+# Team.create(name: "House Crew", members: ["James Fraser" "Ryan Kucera" "Jack Tisdell" "William Ducasse"], tee_off_time: 1034, score: 0)
+# Team.create(name: "Alcoholics Anonymous", members: ["Dario Baccalaro" "Shane Chappus" "Noah Gareau" "N/A"], tee_off_time: 1042, score: 0)
 
 # Team.create(name: "Golden Oldies", member_1: "Chris Mapp", member_2: "Mark Uchwat", member_3: "Jim Ross", member_4: "Bill Gregory", tee_off_time: 930, score: 0)
 # Team.create(name: "Uchwat Family", member_1: "Unknown 1", member_2: "Unknown 2", member_3: "Unknown 3", member_4: "Unknown 4", tee_off_time: 938, score: 0)
