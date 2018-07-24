bc6b4b5fbf05b4b17c7720589022f7ab89c5dade
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-07-24 13:22:05 -0400

    Re-Edited the Seed, Diplaying Properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,83 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts 'Cleaning database...'
+puts 'Cleaning Database...'
 Member.destroy_all
 Team.destroy_all
 
-puts 'Creating teams...'
+puts 'Creating Teams...'
 
+puts 'Creating "Golden Oldies"'
 golden_oldies = Team.create!(name: "Golden Oldies", tee_off_time: 930, score: 0)
 Member.create!(team: golden_oldies, first_name: "Chris", last_name: "Mapp")
 Member.create!(team: golden_oldies, first_name: "Mark", last_name: "Uchwat")
 Member.create!(team: golden_oldies, first_name: "Jim", last_name: "Ross")
 Member.create!(team: golden_oldies, first_name: "Bill", last_name: "Gregory")
 
-# Team.create(name: "Uchwat Family", members: ["Unknown 1" "Unknown 2" "Unknown 3" "Unknown 4"], tee_off_time: 938, score: 0)
-# Team.create(name: "The Organizers", members: ["Arthur Burczak" "Sebastien Jacques" "Derek Pickford" "LJ Aguinaga"], tee_off_time: 946, score: 0)
-# Team.create(name: "Mish-Mash", members: ["Alex Corrado" "Carlos Da Costa Frias" "Cody Lieberman" "Colin Mackenzie"], tee_off_time: 954, score: 0)
-# Team.create(name: "Order of Former CPA's of Quebec", members: ["Artour Goldenberg" "Pascal Karel Matte" "Kirby Coussa" "David J. Stein"], tee_off_time: 1002, score: 0)
-# Team.create(name: "SP Caravan", members: ["Benjamin Mason" "Ardavan Hosseini" "Adrian Payne" "Sajith Rupasingha"], tee_off_time: 1010, score: 0)
-# Team.create(name: "The Swordsmiths", members: ["Yasin Ismail" "Alex Le" "Nicholas Jasinski" "Alexandre Duchesne"], tee_off_time: 1018, score: 0)
-# Team.create(name: "SP Caravan, The Sequel", members: ["Andrew Brand" "Arun Aggarwal-Schifellite" "Christian Chan" "Scott McGee"], tee_off_time: 1026, score: 0)
-# Team.create(name: "House Crew", members: ["James Fraser" "Ryan Kucera" "Jack Tisdell" "William Ducasse"], tee_off_time: 1034, score: 0)
+puts 'Creating "Uchwat Family"'
+uchwat_family = Team.create!(name: "Uchwat Family", tee_off_time: 938, score: 0)
+Member.create!(team: uchwat_family, first_name: "Unknown", last_name: "1")
+Member.create!(team: uchwat_family, first_name: "Unknown", last_name: "2")
+Member.create!(team: uchwat_family, first_name: "Unknown", last_name: "3")
+Member.create!(team: uchwat_family, first_name: "Unknown", last_name: "4")
+
+puts 'Creating "The Organizers"'
+the_organizers = Team.create!(name: "The Organizers", tee_off_time: 946, score: 0)
+Member.create!(team: the_organizers, first_name: "Arthur", last_name: "Burczak")
+Member.create!(team: the_organizers, first_name: "Sébastien", last_name: "Jacques")
+Member.create!(team: the_organizers, first_name: "Derek", last_name: "Pickford")
+Member.create!(team: the_organizers, first_name: "LJ", last_name: "Aguinaga")
+
+puts 'Creating "The 4 C\'s-ons"'
+four_cs = Team.create!(name: "The 4 C's-ons", tee_off_time: 954, score: 0)
+Member.create!(team: four_cs, first_name: "Alex", last_name: "Corrado")
+Member.create!(team: four_cs, first_name: "Carlos", last_name: "Da Costa Frias")
+Member.create!(team: four_cs, first_name: "Cody", last_name: "Lieberman")
+Member.create!(team: four_cs, first_name: "Colin", last_name: "Mackenzie")
+
+puts 'Creating "Order of Former CPA\'s of Quebec"'
+former_cpas = Team.create!(name: "Order of Former CPA's of Quebec", tee_off_time: 1002, score: 0)
+Member.create!(team: former_cpas, first_name: "Artour", last_name: "Goldenberg")
+Member.create!(team: former_cpas, first_name: "Pascal Karel", last_name: "Matte")
+Member.create!(team: former_cpas, first_name: "Kirby", last_name: "Coussa")
+Member.create!(team: former_cpas, first_name: "David J.", last_name: "Stein")
+
+puts 'Creating "SP Caravan"'
+sp_caravan = Team.create!(name: "SP Caravan", tee_off_time: 1010, score: 0)
+Member.create!(team: sp_caravan, first_name: "Benjamin", last_name: "Mason")
+Member.create!(team: sp_caravan, first_name: "Ardavan", last_name: "Hosseini")
+Member.create!(team: sp_caravan, first_name: "Adrian", last_name: "Payne")
+Member.create!(team: sp_caravan, first_name: "Sajith", last_name: "Rupasingha")
+
+puts 'Creating "The Swordsmiths"'
+the_swordsmiths = Team.create!(name: "The Swordsmiths", tee_off_time: 1018, score: 0)
+Member.create!(team: the_swordsmiths, first_name: "Yasin", last_name: "Ismail")
+Member.create!(team: the_swordsmiths, first_name: "Alex", last_name: "Le")
+Member.create!(team: the_swordsmiths, first_name: "Nicholas", last_name: "Jasinski")
+Member.create!(team: the_swordsmiths, first_name: "Alexandre", last_name: "Duchesne")
+
+puts 'Creating "SP Caravan, The Sequel"'
+caravan_sequel = Team.create!(name: "SP Caravan, The Sequel", tee_off_time: 1026, score: 0)
+Member.create!(team: caravan_sequel, first_name: "Andrew", last_name: "Brand")
+Member.create!(team: caravan_sequel, first_name: "Arun", last_name: "Aggarwal-Schifellite")
+Member.create!(team: caravan_sequel, first_name: "Christian", last_name: "Chan")
+Member.create!(team: caravan_sequel, first_name: "Scott", last_name: "McGee")
+
+puts 'Creating "House Crew"'
+house_crew = Team.create!(name: "House Crew", tee_off_time: 1034, score: 0)
+Member.create!(team: house_crew, first_name: "James", last_name: "Fraser")
+Member.create!(team: house_crew, first_name: "Ryan", last_name: "Kucera")
+Member.create!(team: house_crew, first_name: "Jack", last_name: "Tisdell")
+Member.create!(team: house_crew, first_name: "William", last_name: "Ducasse")
+
+puts 'Creating "Alcoholics Anonymous"'
+alco_anonymous = Team.create!(name: "Alcoholics Anonymous", tee_off_time: 1034, score: 0)
+Member.create!(team: alco_anonymous, first_name: "Dario", last_name: "Baccalaro")
+Member.create!(team: alco_anonymous, first_name: "Shane", last_name: "Chappus")
+Member.create!(team: alco_anonymous, first_name: "Noah", last_name: "Gareau")
+
 # Team.create(name: "Alcoholics Anonymous", members: ["Dario Baccalaro" "Shane Chappus" "Noah Gareau" "N/A"], tee_off_time: 1042, score: 0)
 
-
-# Team.create(name: "Golden Oldies", member_1: "Chris Mapp", member_2: "Mark Uchwat", member_3: "Jim Ross", member_4: "Bill Gregory", tee_off_time: 930, score: 0)
-# Team.create(name: "Uchwat Family", member_1: "Unknown 1", member_2: "Unknown 2", member_3: "Unknown 3", member_4: "Unknown 4", tee_off_time: 938, score: 0)
-# Team.create(name: "The Organizers", member_1: "Arthur Burczak", member_2: "Sebastien Jacques", member_3: "Derek Pickford", member_4: "LJ Aguinaga", tee_off_time: 946, score: 0)
-# Team.create(name: "Mish-Mash", member_1: "Alex Corrado", member_2: "Carlos Da Costa Frias", member_3: "Cody Lieberman", member_4: "Colin Mackenzie", tee_off_time: 954, score: 0)
-# Team.create(name: "Order of Former CPA's of Quebec", member_1: "Artour Goldenberg", member_2: "Pascal Karel Matte", member_3: "Cody Lieberman", member_4: "Colin Mackenzie", tee_off_time: 1002, score: 0)
-# Team.create(name: "SP Caravan", member_1: "Benjamin Mason", member_2: "Ardavan Hosseini", member_3: "Adrian Payne", member_4: "Sajith Rupasingha", tee_off_time: 1010, score: 0)
-# Team.create(name: "The Swordsmiths", member_1: "Yasin Ismail", member_2: "Alex Le", member_3: "Nicholas Jasinski", member_4: "Alexandre Duchesne", tee_off_time: 1018, score: 0)
-# Team.create(name: "SP Caravan, The Sequel", member_1: "Andrew Brand", member_2: "Arun Aggarwal-Schifellite", member_3: "Christian Chan", member_4: "Scott McGee", tee_off_time: 1026, score: 0)
-# Team.create(name: "House Crew", member_1: "James Fraser", member_2: "Ryan Kucera", member_3: "Jack Tisdell", member_4: "William Ducasse", tee_off_time: 1034, score: 0)
-# Team.create(name: "Alcoholics Anonymous", member_1: "Dario Baccalaro", member_2: "Shane Chappus", member_3: "Noah Gareau", member_4: "N/A", tee_off_time: 1042, score: 0)
-
-puts 'Bro best golf tournament bro!'
+puts 'Teams Loaded'
+puts 'Keep it clean, now come out golfing!'
 
