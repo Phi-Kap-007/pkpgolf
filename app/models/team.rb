feaf6ab1a592e8725d61fd9eecba0621acfc092b
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-08-01 19:47:28 -0400

    Martin Magic: FORM FINALLY WORKS! Also Updated User & Team Models, Added Controller Actions for Users

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,11 @@ class Team < ApplicationRecord
   has_many :users
   validates :name, presence: true
   validates :users, length: { maximum: 4 }
-  validates :score, numericality: true
+  # validates :score, numericality: true
+
+  validate :validate_number_of_users
+
+  def validate_number_of_users
+    errors.add(:users, "too much") if users.size > 4
+  end
 end
