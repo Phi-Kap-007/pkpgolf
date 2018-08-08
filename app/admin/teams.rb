a0181503fde02525e759d3d842c06266cb006854
Alex Corrado alexcorrado22@gmail.com <alexcorrado22@gmail.com>
2018-08-07 23:58:10 -0400

    Added Admin User & Team Models, STILL Cannot Access Admin Page...

---
+++ app/admin/teams.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Team do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+  actions :all, except: [:update, :destroy]
+
+  permit_params do
+    permitted = [:name, :tee_off_time]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  controller do
+    def create
+      # Good
+      @team = Team.new(permitted_params[:team])
+
+      if @team.save
+        # Don't know if that's correct...
+        redirect_to team_path(@team)
+      end
+    end
+  end
+end
---
+++ app/admin/users.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+  actions :all, except: [:update, :destroy]
+
+  permit_params do
+    permitted = [:first_name, :last_name, :total_score, :score_hole_1, :score_hole_2,
+      :score_hole_3, :score_hole_4, :score_hole_5, :score_hole_6, :score_hole_7,
+      :score_hole_8, :score_hole_9, :score_first_half, :score_hole_10, :score_hole_11,
+      :score_hole_12, :score_hole_13, :score_hole_14, :score_hole_15, :score_hole_16,
+      :score_hole_17, :score_hole_18, :score_second_half]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  controller do
+    def create
+      # Good
+      @user = User.new(permitted_params[:user])
+
+      if @user.save
+        # Don't know if that's correct...
+        redirect_to teams_path
+      end
+    end
+  end
+end
